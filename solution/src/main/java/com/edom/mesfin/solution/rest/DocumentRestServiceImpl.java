5e578f29b8f02f9a2c60133c41259cdb9a4928ff
Edom Mesfin <edom.mesfin.cs@gmail.com>
2017-10-04 14:16:03 -0400

    Just minor addition. Haven't got time yesterday. Download feature not requested, I'm just adding it since I got some time now.

--- solution/src/main/java/com/edom/mesfin/solution/rest/DocumentRestServiceImpl.java
+++ solution/src/main/java/com/edom/mesfin/solution/rest/DocumentRestServiceImpl.java
@@ -7,15 +7,25 @@ import com.edom.mesfin.solution.service.DocumentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.UUID;
+//import javax.annotation.Resource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ByteArrayResource;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * Service Implementation Class
@@ -26,6 +36,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class DocumentRestServiceImpl implements DocumentRestService{
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(DocumentRestServiceImpl.class);
+    
+    private final String FILE_DIR = System.getProperty("user.home") + File.separator + "file_dir";
     
     @Autowired
     private DocumentService docServImpl;
@@ -65,10 +77,10 @@ public class DocumentRestServiceImpl implements DocumentRestService{
         docServImpl.createOrupdate(doc);
 
         ////Then Save file to local file system using UUID and file extension for later retrival.                
-        File file;
-        try {
-            file = File.createTempFile(uuid, null);
-            file.deleteOnExit();
+        File file;        
+        String ext = FilenameUtils.getExtension(uploadFile.getOriginalFilename());
+        file = new File(FILE_DIR + File.separator + uuid + "." + ext);
+        try {                        
             FileUtils.copyInputStreamToFile(uploadFile.getInputStream(), file);
         } catch (IOException ex) {
             errMsg = "Error persisting the file stream to local file.";
@@ -80,12 +92,41 @@ public class DocumentRestServiceImpl implements DocumentRestService{
     }
 
     @Override
-    public ResponseEntity<String> getFileByuuidSysName(String uuid, String sysName) {
-        
+    public ResponseEntity<Resource> getFileByuuidSysName(String uuid, String sysName) {
+        String errMsg;
         ////Return file. Not requested.
-        // ...
-        return ResponseEntity.ok().body("Document Uploaded successfully! UUID:" + uuid);
         
+        Document doc = docServImpl.getDocumentByUuid(uuid);
+        if(doc == null){
+            errMsg = "Document could not be found for the given UUID: " + uuid;
+            LOG.debug(errMsg);
+            return ResponseEntity.notFound().header("ErrorMsg", errMsg).build();
+        }
+        
+        String ext = FilenameUtils.getExtension(doc.getFileName());
+        File file = new File(FILE_DIR + File.separator + uuid + "." + ext);
+        
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource;
+        try {
+            resource = new ByteArrayResource(Files.readAllBytes(path));
+        } catch (IOException ex) {
+            errMsg = "Document meta-data was found, but the actual file could not be retrieved from the local file system. UUID: " + uuid;
+            LOG.debug(errMsg);
+            return ResponseEntity.notFound().header("ErrorMsg", errMsg).build();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("UUID", doc.getUuid());
+        headers.add("FileName", doc.getFileName());
+        headers.add("SystemName", doc.getSysName());
+        headers.add("Created_Date", doc.getCreatedDate().toString());
+        headers.add("Content-Disposition", "attachment; filename=\"" + doc.getFileName() + "\"");
+        
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);        
     }
 
 }
